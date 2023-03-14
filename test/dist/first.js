f46ecb76504e25a752ac9db38228dbff16c0b428
dejanV91 <dejanvukoicic91@gmail.com>
2023-03-14 15:57:23 +0100

    finish beginner course

--- test/dist/first.js
+++ test/dist/first.js
@@ -1,7 +1,47 @@
 "use strict";
-let age = 20;
-if (age < 50) {
-    age += 10;
+let skup = [1, 2, 4];
+let tuple = ["apple", 2];
+let value = 1;
+function calculateTax(income, taxYear = 8) {
+    if (taxYear > 50000) {
+        return income * 0.2;
+    }
+    return income;
 }
-console.log(age);
+const employee = {
+    id: 3,
+    retire: (data) => {
+        console.log(data);
+    },
+};
+employee.name = "Mark";
+function kgToLbs(weight) {
+    if (typeof weight === "number") {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+kgToLbs(10);
+kgToLbs("10kg");
+let box = {
+    width: () => { },
+    height: () => { },
+};
+let quanrity = 100;
+function greet(name) {
+    if (name) {
+        console.log(name.toUpperCase());
+    }
+    else {
+        console.log("Hay");
+    }
+}
+greet(null);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday.getFullYear());
 //# sourceMappingURL=first.js.map
--- test/src/first.ts
+++ test/src/first.ts
@@ -1,5 +1,99 @@
-let age: number = 20;
-if (age < 50) {
-  age += 10;
+let skup: number[] = [1, 2, 4];
+
+// tuples
+let tuple: [string, number] = ["apple", 2];
+
+// enum
+const enum Size {
+  Small = "a",
+  Medium = 1,
+  Large,
 }
-console.log(age);
+
+let value: Size = Size.Medium;
+
+//functions
+function calculateTax(income: number, taxYear = 8): number {
+  if (taxYear > 50000) {
+    return income * 0.2;
+  }
+  return income;
+}
+
+//Objects and type aliases
+
+type Employee = {
+  readonly id: number;
+  name?: string;
+  retire: (data: Date) => void;
+};
+
+const employee: Employee = {
+  id: 3,
+  retire: (data) => {
+    console.log(data);
+  },
+};
+
+employee.name = "Mark";
+
+// Union type
+
+function kgToLbs(weight: number | string): number {
+  if (typeof weight === "number") {
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
+  }
+}
+
+kgToLbs(10);
+kgToLbs("10kg");
+
+// Intersection types
+type Width = {
+  width: () => void;
+};
+type Height = {
+  height: () => void;
+};
+
+type Area = Height & Width;
+
+let box: Area = {
+  width: () => {},
+  height: () => {},
+};
+
+// Literal Types
+type Quantity = 50 | 100;
+let quanrity: Quantity = 100;
+
+type Metric = "cm" | "inch";
+
+// Nullable types
+
+function greet(name: string | null | undefined) {
+  if (name) {
+    console.log(name.toUpperCase());
+  } else {
+    console.log("Hay");
+  }
+}
+greet(null);
+
+// Optional chaining
+
+type Customer = {
+  birthday: Date;
+};
+
+function getCustomer(id: number): Customer | null {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(0);
+console.log(customer?.birthday.getFullYear());
+
+let log: any = null;
+log?.("a");
